59851b6ed9470edcf8321ea8545fa9bdca883af3
Daniel Nau <71359236+dannauu@users.noreply.github.com>
2022-12-06 13:54:14 -0600

    structured navbar and added some css for the logo/initial commit

---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Navigation.css'
+
+const Navigation = () => {
+  return (
+    <nav className='flex flex-row'>
+        <h1 className='text-4xl basis-1/2 text-center'>TAXES <span className='logoText text-2xl'>BY</span> ME.TJR</h1>
+        <button className='text-3xl basis-1/2 text-center'>816-690-7997</button>
+    </nav>
+  )
+}
+
+export default Navigation
