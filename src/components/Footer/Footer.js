a221daafe65289199e3001ee41c3ee5eacabf198
Daniel Nau <71359236+dannauu@users.noreply.github.com>
2022-12-19 14:36:22 -0600

    added signature to footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,8 +5,8 @@ const Footer = () => {
 
         <footer className="m-5">
             <div className="text-center">
-                <h1 className='text-base font-bold sm:text-sm'>© 2022 TAXES <span className='logoText text-xs'>BY</span> ME.TJR&trade;. All Rights Reserved
-                </h1>
+                <h1 className='text-base font-bold sm:text-sm'>© 2022 TAXES <span className='logoText text-xs'>BY</span> ME.TJR&trade;. All Rights Reserved</h1>
+                <h2 className='text-xs italic'>Website built by Daniel Nau</h2>
             </div>
         </footer>
 
